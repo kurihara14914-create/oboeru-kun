531dc5023f487fc6a60ec20f75136794a990b9a2
kurihara14914-create <kurihara14914-create@users.noreply.github.com>
2026-05-31 06:03:30 +0900

    Recolor delete actions to grayscale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "oboeru-kun-v18";
+const CACHE_NAME = "oboeru-kun-v19";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
