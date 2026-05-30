822cbb2c4c2de87ddc2c945de2587baeec3abf1d
kurihara14914-create <kurihara14914-create@users.noreply.github.com>
2026-05-31 06:11:48 +0900

    Soften delete buttons and enlarge key actions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "oboeru-kun-v20";
+const CACHE_NAME = "oboeru-kun-v21";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
